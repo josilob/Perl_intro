929fc2ae95043570b0541f2f6e07b18659ad50ed
Bojan Josilo <bjosilo@gmail.com>
2022-04-07 21:22:03 -0500

    complete strings

--- perl_strings.pl
+++ perl_strings.pl
@@ -20,10 +20,10 @@ say "Index 7 through 10 ", substr $long_string, 7, 4;
 # word 'long' substr(word,inclusive_idx,exclusive_idx)
 
 my $animal = "animals";
-# chop deletes and returns the last Character
+# chop deletes and returns the LAST CHARACTER
 printf("Last character is %s \n", chop $animal);
 
-# chomp deletes the last newline
+# chomp deletes the LAST NEWLINE
 my $no_newline = "No Newline\n";
 chomp $no_newline;
 say $no_newline;
@@ -32,3 +32,25 @@ say $no_newline;
 printf("Uppercase : %s \n", uc $long_string);
 printf("Lowercase : %s \n", lc $long_string);
 printf("1st Uppercase : %s \n", ucfirst $long_string);
+
+# REPLACE (s///)
+
+# Must have ~ after =
+$long_string =~ s/ /, /g; # replace all spaces with comma-spaces
+say $long_string;
+
+# Repeat a string
+my $two_times_string = $long_string x 2;
+say $two_times_string; # repeats string twice
+
+# RANGES (of letters or numbers)
+my @abcs = ('a' .. 'z');
+my @abcs = (1 .. 12);
+say @abcs;
+
+print join(", ", @abcs), "\n"; # create a string by joining list elements by first passed argument
+# join(joining_character, list)
+
+# possible to increment letters as well as numbers
+my $letter = 'a';
+say ++$letter; # prints 'b'
