b3734ad4a2aef37fc50dc566b33c27a38bbe11ce
Bojan Josilo <bjosilo@gmail.com>
2022-04-07 20:58:11 -0500

    looping in perl

--- perl_conditionals.pl
+++ perl_conditionals.pl
@@ -44,3 +44,52 @@ unless(!$is_not_intoxicated){
 # Ternary operator returns different values depending on a condition
 say (($age > 18) ? "Can Vote" : "Can't Vote");
 
+# ---------- LOOPING ----------
+
+# For loop
+for(my $i = 0; $i < 10; $i++){
+  say $i;
+}
+
+# Print odds with the While loop
+my $i = 1;
+
+while ($i < 10){
+  if($i % 2 == 0){
+    $i++;
+
+    # next skips back to the beginning of the loop
+    next;
+  }
+
+  # Last exits out of the loop
+  if($i == 7){ last; }
+
+  say $i;
+  $i++;
+}
+
+# The Do while loop is used when you must loop once
+my $lucky_num = 7;
+my $guess;
+do {
+  say "Guess a Number Between 1 and 10";
+
+  # This is how you get user input
+  $guess = <STDIN>;
+} while $guess != $lucky_num;
+
+say "You guessed 7";
+
+# Given When Perl Switch statement
+my $age_old = 18;
+given ($age_old) {
+  when($_ > 16) {
+    say "Drive";
+
+    # Will continue with the next cases
+    continue;
+  }
+  when($_ > 17) {say "Go Vote";}
+  default {say "Nothing Special";}
+}
