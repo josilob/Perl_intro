3c596755bda5fa2b866e0a37f5f6dd07115743c2
Bojan Josilo <bjosilo@gmail.com>
2022-04-11 21:35:11 -0500

    array iterating

--- perl_arrays.pl
+++ perl_arrays.pl
@@ -16,3 +16,28 @@ $my_info[4] = "Josilo";
 
 # Access array items by index starting at 0:
 say $my_info[4];
+
+# Iterating through the array
+for my $info (@my_info){
+  say $info;
+}
+
+# foreach cycles through an array
+foreach my $num (@primes){
+  say "Prime : ", $num;
+}
+
+# $_ is automatically used if no variable is declared
+for (@my_info){
+  say $_;
+}
+
+# Slice data from an array
+# ("Bojan", "123 Main St", 27, 6.25, "Josilo");
+my @my_name = @my_info[0, 4];
+say @my_name; # BojanJosilo
+
+# When scalar is used on an array it returns the length of the array
+my $items = scalar @my_info;
+# say $items; # 5
+
