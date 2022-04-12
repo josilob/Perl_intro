1c01ab095599593174d857fae6be99a1f9e5d74d
Bojan Josilo <bjosilo@gmail.com>
2022-04-11 21:42:06 -0500

    array methods

--- perl_arrays.pl
+++ perl_arrays.pl
@@ -41,3 +41,56 @@ say @my_name; # BojanJosilo
 my $items = scalar @my_info;
 # say $items; # 5
 
+# Assign values from array to variables
+my ($f_name, $address, $how_old, $height, $l_name) = @my_info;
+say "$f_name $l_name";
+
+# Pop the last value off an array
+say "Popped Value ", pop @primes;
+
+# Push puts one on the end and returns the length
+say "Pushed Value ", push @primes, 17;
+print join(", ", @primes), "\n";
+
+# Return the first item with shift
+say "First Item ", shift @primes;
+
+# Add a value to the front and get the length
+say "Unshifted Item ", unshift @primes, 2;
+print join(", ", @primes), "\n";
+
+# Splice out values array, index to start, length
+# Returns those values
+say "Remove Index 0 - 2 ", splice @primes, 0, 3;
+print join(", ", @primes), "\n";
+
+# Join can also join a list like this
+print join " ", ('list', 'of', 'words', "\n");
+
+# Split turns a string into an array
+my $customers = "Sue Sally Paul";
+my @cust_array = split / /, $customers;
+print join(", ", @cust_array), "\n";
+
+# Reverse reverses an array
+@cust_array = reverse @cust_array;
+print join(", ", @cust_array), "\n";
+
+# Sort sorts an array
+@cust_array = sort @cust_array;
+print join(", ", @cust_array), "\n";
+
+# Sort in reverse order
+@cust_array = reverse sort @cust_array;
+print join(", ", @cust_array), "\n";
+
+# Grep filters a list according to an expression
+my @number_array = (1,2,3,4,5,6,7,8);
+
+# Adds the value if modulus operation doesn't return 0
+my @odds_array = grep {$_ % 2} @number_array;
+print join(", ", @odds_array), "\n";
+
+# Map performs a function on every item
+my @dbl_array = map {$_ * 2} @number_array;
+print join(", ", @dbl_array), "\n";
